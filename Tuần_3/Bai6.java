766fcf2f31b207c4447c364efa818d74c38c8a9d
Nqv-1208 <qvinhkl10@gmail.com>
2024-10-13 17:06:11 +0700

    Update Bài tập tuần 3

---
+++ Tuần_3/Bai6.java
@@ -0,0 +1,47 @@
+package Tuần_3;
+
+import java.util.Scanner;
+
+public class Bai6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double result;
+
+        System.out.print("Nhap so thu nhat: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Nhap so thu hai: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Chon phep tinh (+, -, *, /): ");
+        char Tinh = scanner.next().charAt(0);
+ 
+        switch (Tinh) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Ket qua: " + num1 + " + " + num2 + " = " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Ket qua: " + num1 + " - " + num2 + " = " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Ket qua: " + num1 + " * " + num2 + " = " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Ket qua: " + num1 + " / " + num2 + " = " + result);
+                } else {
+                    System.out.println("Khong the chia cho 0.");
+                }
+                break;
+            default:
+                System.out.println("Phep toan khong hop le.");
+                break;
+        }
+        scanner.close();
+    }
+}
+ 
+ 
+     
